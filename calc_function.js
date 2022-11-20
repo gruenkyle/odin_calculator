0947b8e352196cb2e8701b5b915c043419be55bc
gruenkyle <gruenkyle@vt.edu>
2022-11-20 00:06:21 -0500

    Partial Functionality
    -Calculator Css Done
    
    Javascript Operations
    
    -HTML Organizations

--- calc_function.js
+++ calc_function.js
@@ -27,7 +27,56 @@ function operate(a, operator, b) {
     else if (operator == '/') {
         return divide(a, b);
     }
-    else{
-        return "Not Working"; 
+    else {
+        return "Not Working";
     }
 };
+
+let currentValue = '';
+let previousValue = '';
+let operator = '';
+
+const numbers = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const inputBar = document.querySelector(".input-text");
+const equal = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+
+numbers.forEach((number) => number.addEventListener("click", function (e) {
+    handleNumber(e.target.value);
+    inputBar.textContent = currentValue;
+}));
+
+operators.forEach((op) => op.addEventListener("click", function (e) {
+    handleOperator(e.target.value);
+    inputBar.textContent = (`${previousValue} ${e.target.value} ${currentValue}`);
+
+}));
+
+function handleNumber(num) {
+    currentValue += num;
+    console.log(num);
+}
+
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+
+    console.log(op);
+}
+
+clear.addEventListener("click", function(){
+    previousValue = '';
+    currentValue = '';
+    operator = ''; 
+    inputBar.textContent = ''; 
+});
+
+equal.addEventListener("click", function(){
+    let finalAnswer = operate(previousValue, operator, currentValue);
+    inputBar.textContent = (`${finalAnswer}`); 
+});
+
+
+
